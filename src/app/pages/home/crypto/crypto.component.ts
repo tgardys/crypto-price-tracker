87788e20fe319ac834769460ac2269ca24a95f07
mswiatczak <michalswiatczakk@gmail.com>
2022-11-05 14:47:54 +0000

    crypto page template

--- src/app/pages/home/crypto/crypto.component.ts
+++ src/app/pages/home/crypto/crypto.component.ts
@@ -1,12 +1,37 @@
 import { Component, OnInit } from '@angular/core';
-
+import { Crypto } from 'src/app/interfaces/crypto.interface';
+import { CryptoData } from 'src/app/interfaces/CryptoData.interface';
+import { RequestService } from 'src/app/services/request.service';
 @Component({
   selector: 'app-crypto',
   templateUrl: './crypto.component.html',
   styleUrls: ['./crypto.component.scss'],
 })
 export class CryptoComponent implements OnInit {
-  constructor() {}
+  cryptos: Crypto[] = [];
+  cryptoData: CryptoData[] = [];
 
-  ngOnInit() {}
+  isLoading = true;
+
+  constructor(private cryptoService: RequestService) {}
+
+  ngOnInit(): void {
+    this.onGetCrypto();
+  }
+
+  AfterViewInit(): void {}
+
+  onGetCrypto(): void {
+    this.cryptoService.getCrypto().subscribe({
+      next: (response) => {
+        this.cryptos = response;
+        console.log('crypto list: ', this.cryptos);
+
+        this.cryptoData = this.cryptos.map((quote) => quote.quote.USD);
+        console.log('crypto data: ', this.cryptoData);
+      },
+      error: (error: any) => console.log('error: ', error),
+      complete: () => (this.isLoading = false),
+    });
+  }
 }
