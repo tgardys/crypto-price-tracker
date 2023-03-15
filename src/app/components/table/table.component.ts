deb6df404e89e47a8d4f2a0ad11b5750ce0d7d37
mswiatczak <michalswiatczakk@gmail.com>
2023-03-15 17:32:00 +0000

    table sorting

--- src/app/components/table/table.component.ts
+++ src/app/components/table/table.component.ts
@@ -11,6 +11,7 @@ export class TableComponent implements OnInit {
   cryptoData: any;
   itemsPerPage: number = 25;
   currentPage: number = 1;
+  orderDSC: boolean = true;
   constructor(private cryptoService: RequestService, public router: Router) {}
 
   ngOnInit(): void {
@@ -31,5 +32,88 @@ export class TableComponent implements OnInit {
         console.log('error while fetching crypto market data: ', error),
       complete: () => (this.isLoading = false),
     });
+  }
+
+  sortPrice() {
+    if (this.orderDSC) {
+      let newarr = this.cryptoData.sort(
+        (a: { current_price: number }, b: { current_price: number }) =>
+          b.current_price - a.current_price
+      );
+      this.cryptoData = newarr;
+    } else {
+      let newarr = this.cryptoData.sort(
+        (a: { current_price: number }, b: { current_price: number }) =>
+          a.current_price - b.current_price
+      );
+      this.cryptoData = newarr;
+    }
+    this.orderDSC = !this.orderDSC;
+  }
+  sortMarketCap() {
+    if (this.orderDSC) {
+      let newarr = this.cryptoData.sort(
+        (a: { market_cap: number }, b: { market_cap: number }) =>
+          b.market_cap - a.market_cap
+      );
+      this.cryptoData = newarr;
+    } else {
+      let newarr = this.cryptoData.sort(
+        (a: { market_cap: number }, b: { market_cap: number }) =>
+          a.market_cap - b.market_cap
+      );
+      this.cryptoData = newarr;
+    }
+    this.orderDSC = !this.orderDSC;
+  }
+
+  sort24hChange() {
+    if (this.orderDSC) {
+      let newarr = this.cryptoData.sort(
+        (
+          a: { price_change_percentage_24h: number },
+          b: { price_change_percentage_24h: number }
+        ) => b.price_change_percentage_24h - a.price_change_percentage_24h
+      );
+      this.cryptoData = newarr;
+    } else {
+      let newarr = this.cryptoData.sort(
+        (
+          a: { price_change_percentage_24h: number },
+          b: { price_change_percentage_24h: number }
+        ) => a.price_change_percentage_24h - b.price_change_percentage_24h
+      );
+      this.cryptoData = newarr;
+    }
+    this.orderDSC = !this.orderDSC;
+  }
+
+  sortName() {
+    if (this.orderDSC) {
+      let newarr = this.cryptoData.sort(function (a: any, b: any) {
+        let nameA = a.name.toLowerCase();
+        let nameB = b.name.toLowerCase();
+        if (nameA < nameB) {
+          return -1;
+        }
+        if (nameA > nameB) {
+          return 1;
+        }
+        return 0;
+      });
+    } else {
+      let newarr = this.cryptoData.sort(function (a: any, b: any) {
+        let nameA = a.name.toLowerCase();
+        let nameB = b.name.toLowerCase();
+        if (nameA < nameB) {
+          return 1;
+        }
+        if (nameA > nameB) {
+          return -1;
+        }
+        return 0;
+      });
+    }
+    this.orderDSC = !this.orderDSC;
   }
 }
